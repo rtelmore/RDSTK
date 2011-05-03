ee694ef5de34df97bcfdc2e176e47067855e4bf1
Ryan Elmore <rtelmore@gmail.com>
2011-05-03 10:51:23 -0600

    added getCurlHandle to arguments of functions

--- src/RDSTK/R/functions.R
+++ src/RDSTK/R/functions.R
@@ -2,56 +2,60 @@
 ## Date: 24 April 2011
 ## Include project-specific functions in this file
 
-street2coordinates <- function(address) {
+street2coordinates <- function(address, session=getCurlHandle()) {
   api <- "http://www.datasciencetoolkit.org/street2coordinates/"
-  get.addy <- getURL(paste(api, URLencode(address), sep=""))
+  get.addy <- getURL(paste(api, URLencode(address), sep=""), curl=session)
   result <- ldply(fromJSON(get.addy), data.frame)
   names(result)[1] <- "full.address"
   return(result)
 }
 
-ip2coordinates <- function(ip) {
+ip2coordinates <- function(ip, session=getCurlHandle()) {
   api <- "http://www.datasciencetoolkit.org/ip2coordinates/"
-  get.ips <- getURL(paste(api, URLencode(ip), sep="")) 
+  get.ips <- getURL(paste(api, URLencode(ip), sep=""), curl=session) 
   result <- ldply(fromJSON(get.ips), data.frame)
   names(result)[1] <- "ip.address"
   return(result)
 }
 
-coordinates2politics <- function(latitude, longitude) {
+coordinates2politics <- function(latitude, longitude, session=getCurlHandle()) {
   api <- "http://www.datasciencetoolkit.org/coordinates2politics/"
-  result <- getURL(paste(api, latitude, "%2c", longitude, sep=""))
+  result <- getURL(paste(api, latitude, "%2c", longitude, sep=""), curl=session)
   return(result)
 }
 
-text2sentences <- function(text) {
+text2sentences <- function(text, session=getCurlHandle()) {
   api <- "http://www.datasciencetoolkit.org/text2sentences"
   r = dynCurlReader()
-  curlPerform(postfields=text, url=api, post=1L, writefunction=r$update)
+  curlPerform(postfields=text, url=api, post=1L, writefunction=r$update,
+              curl=session)
   result <- fromJSON(r$value())
   return(result)
 }
 
-text2people <- function(text) {
+text2people <- function(text, session=getCurlHandle()) {
   api <- "http://www.datasciencetoolkit.org/text2people"
   r = dynCurlReader()
-  curlPerform(postfields=text, url=api, post=1L, writefunction=r$update)
+  curlPerform(postfields=text, url=api, post=1L, writefunction=r$update,
+              curl=session)
   result <- ldply(fromJSON(r$value()), data.frame)
   return(result)
 }
 
-html2text <- function(html) {
+html2text <- function(html, session=getCurlHandle()) {
   api <- "http://www.datasciencetoolkit.org/html2text"
   r = dynCurlReader()
-  curlPerform(postfields=html, url=api, post=1L, writefunction=r$update)
+  curlPerform(postfields=html, url=api, post=1L, writefunction=r$update, 
+              curl=session)
   result <- fromJSON(r$value())
   return(result)
 }
 
-text2times <- function(text) {
+text2times <- function(text, session=getCurlHandle()) {
   api <- "http://www.datasciencetoolkit.org/text2times"
   r = dynCurlReader()
-  curlPerform(postfields=text, url=api, post=1L, writefunction=r$update)
+  curlPerform(postfields=text, url=api, post=1L, writefunction=r$update,
+              curl=session)
   result <- ldply(fromJSON(r$value()), data.frame)
   return(result)
 }
